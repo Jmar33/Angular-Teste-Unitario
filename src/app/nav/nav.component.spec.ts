1952e2d3ad04df55b0f5fc77a669cc9f05b3441b
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-09-28 15:18:06 -0300

    Cria teste para componente que utliza rotas

---
+++ src/app/nav/nav.component.spec.ts
@@ -0,0 +1,39 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavComponent } from './nav.component';
+
+class ComponentRouteTest {}
+
+describe('NavComponent', () => {
+  let component: NavComponent, fixture: ComponentFixture<NavComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: 'home',
+            component: ComponentRouteTest,
+          },
+          {
+            path: 'cart',
+            component: ComponentRouteTest,
+          },
+        ]),
+      ],
+      declarations: [NavComponent],
+      providers: [],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
