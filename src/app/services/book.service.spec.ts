7ba1d6d8e8b7adc8a13825c97eb590a8dc937b0b
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-09-28 14:32:40 -0300

    Adiciona testes para métodos que utlizam o localStorage

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -5,6 +5,7 @@ import {
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment.prod';
+import Swal from 'sweetalert2';
 import { Book } from '../models/book.model';
 import { BookService } from './book.service';
 
@@ -32,8 +33,18 @@ const bookList: Book[] = [
   },
 ];
 
+const book = {
+  name: '',
+  author: '',
+  isbn: '',
+  price: 15,
+  amount: 1,
+};
+
 describe('BookService', () => {
-  let service: BookService, httpMocK: HttpTestingController;
+  let service: BookService,
+    httpMocK: HttpTestingController,
+    storage = {};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -46,6 +57,17 @@ describe('BookService', () => {
   beforeEach(() => {
     service = TestBed.inject(BookService);
     httpMocK = TestBed.inject(HttpTestingController);
+
+    storage = {};
+    spyOn(localStorage, 'getItem').and.callFake((key: string) =>
+      storage[key] ? storage[key] : null
+    );
+
+    spyOn(localStorage, 'setItem').and.callFake(
+      (key: string, value: string) => {
+        return (storage[key] = value);
+      }
+    );
   });
 
   afterEach(() => {
@@ -67,5 +89,36 @@ describe('BookService', () => {
 
     // O método flush é um mock que pode ser usado para o retorno de algum método http
     req.flush(bookList);
+  });
+
+  // Por padrão esse método irá acessar diretamente o localStore, o que não é nossa intenção
+  // Já que isso pode acabar poluindo o localStorage, sem necessidade.
+  // Sendo assim criamos um spyOn para o localStorage no método beforeEach
+  it('getBooksFromCart return empty array when localStorage is empty', () => {
+    const listBooks = service.getBooksFromCart();
+    expect(listBooks.length).toBe(0);
+  });
+
+  it('addBookToCart add a book successfully when the list does not exists in the localStorage', () => {
+    const toast = {
+      fire: () => null,
+    } as any;
+    const mixinSpy = spyOn(Swal, 'mixin').and.callFake(() => toast);
+    let listBook = service.getBooksFromCart();
+    expect(listBook.length).toBe(0);
+    service.addBookToCart(book);
+    listBook = service.getBooksFromCart();
+    expect(listBook.length).toBe(1);
+    service.addBookToCart(book);
+    expect(mixinSpy).toHaveBeenCalled();
+  });
+
+  it('removeBooksFromCart removes the list from localStorage', () => {
+    service.addBookToCart(book);
+    let listBook = service.getBooksFromCart();
+    expect(listBook.length).toBe(1);
+    service.removeBooksFromCart();
+    listBook = service.getBooksFromCart();
+    expect(listBook.length).toBe(0);
   });
 });
