748605b59092fe6b4dfaee780126b7bf867184f6
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-09-28 15:42:23 -0300

    Cria teste utilizando o serviço Router de forma mockada

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -6,25 +6,34 @@ import { NavComponent } from './nav.component';
 
 class ComponentRouteTest {}
 
+const routeServiceMock = {
+  navigate: () => null,
+};
+
 describe('NavComponent', () => {
   let component: NavComponent, fixture: ComponentFixture<NavComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: 'home',
-            component: ComponentRouteTest,
-          },
-          {
-            path: 'cart',
-            component: ComponentRouteTest,
-          },
-        ]),
+        // RouterTestingModule.withRoutes([
+        //   {
+        //     path: 'home',
+        //     component: ComponentRouteTest,
+        //   },
+        //   {
+        //     path: 'cart',
+        //     component: ComponentRouteTest,
+        //   },
+        // ]),
       ],
       declarations: [NavComponent],
-      providers: [],
+      providers: [
+        {
+          provide: Router,
+          useValue: routeServiceMock,
+        },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
@@ -38,15 +47,16 @@ describe('NavComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  // Uma solução alternativa para testar um serviço que não conhecemos é
+  // Criar um serviço Mockado
+  // **Atenção: no caso do das rotas sempre devemos dar preferência pelo uso
+  // do RouterTestingModule do que criar um mock, como foi feito nesse exemplo!
   it('should navigate', () => {
     const router = TestBed.inject(Router);
 
     const spyNavigate = spyOn(router, 'navigate');
 
-    component.navTo('home');
-    expect(spyNavigate).toHaveBeenCalledWith(['/home']);
-
-    component.navTo('cart');
-    expect(spyNavigate).toHaveBeenCalledWith(['/cart']);
+    component.navTo('');
+    expect(spyNavigate).toHaveBeenCalled();
   });
 });
