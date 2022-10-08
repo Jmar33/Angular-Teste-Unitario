06dfc4d17f6da21db641384fed8164a0613ab5ee
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-10-08 19:33:54 -0300

    Atualiza projeto

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,92 +1,87 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import {
-  CUSTOM_ELEMENTS_SCHEMA,
-  NO_ERRORS_SCHEMA,
-  Pipe,
-  PipeTransform,
-} from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { Book } from 'src/app/models/book.model';
-import { BookService } from 'src/app/services/book.service';
 import { HomeComponent } from './home.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
+import { of } from 'rxjs';
 
-const bookList: Book[] = [
-  {
-    name: '',
-    author: '',
-    isbn: '',
-    price: 15,
-    amount: 2,
-  },
-  {
-    name: '',
-    author: '',
-    isbn: '',
-    price: 2,
-    amount: 4,
-  },
-  {
-    name: '',
-    author: '',
-    isbn: '',
-    price: 8,
-    amount: 7,
-  },
+const listBook: Book[] = [
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 15,
+        amount: 2
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 20,
+        amount: 1
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 8,
+        amount: 7
+    }
 ];
 
-// Quando implementamos pipes por padrão nosso teste passa a falhar
-// Assim devemos sempre criar um mock do nosso pipe
-@Pipe({
-  name: 'reduceText',
-})
-class ReduceTextPipeMock implements PipeTransform {
-  transform(): string {
-    return '';
-  }
-}
-
 const bookServiceMock = {
-  getBooks: () => of(bookList),
+    getBooks: () => of(listBook),
 };
 
-// Podemos usar os prefixos f e x
-// para poder dar foco ou desabilitar seja um teste ou uma suíte específica
-// f -> usado para dar foco a uma suíte ou teste específico
-// x -> usado para desabilitar uma suíte ou teste específico
+@Pipe({name: 'reduceText'})
+class ReduceTextPipeMock implements PipeTransform {
+    transform(): string {
+        return '';
+    }
+}
 
-describe('Home Component', () => {
-  let component: HomeComponent,
-    fixture: ComponentFixture<HomeComponent>,
-    service: BookService;
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [HomeComponent, ReduceTextPipeMock],
-      providers: [
-        // BookService
-        {
-          provide: BookService,
-          useValue: bookServiceMock,
-        },
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    service = fixture.debugElement.injector.get(BookService);
-  });
+describe('Home component', () => {
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            declarations: [
+                HomeComponent,
+                ReduceTextPipeMock
+            ],
+            providers: [
+                {
+                    provide: BookService,
+                    useValue: bookServiceMock
+                },
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+        }).compileComponents();
+    });
 
-  it('getBooks works correctly', () => {
-    component.getBooks();
-    debugger;
-    expect(component.listBook.length).toBe(3);
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('getBook get books from the subscription', () => {
+        // const bookService = fixture.debugElement.injector.get(BookService);
+        // const spy1 = spyOn(bookService, 'getBooks').and.returnValue(of(listBook));
+        component.getBooks();
+        // expect(spy1).toHaveBeenCalled();
+        expect(component.listBook.length).toBe(3);
+    });
+
+
 });
